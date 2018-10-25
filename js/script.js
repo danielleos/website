16576d7ce043bffb317791a01b55c09fe84a559c
Danielle O'Sullivan <danielleos@users.noreply.github.com>
2018-10-25 16:21:14 +0100

    Fixed messed up bootstrapping (#17)
    * Delete unused SVGs
    * Update contact page
    * Add compatible bootstrap CSS, add progress bars for skills
    * Add inline js to script.js

--- js/script.js
+++ js/script.js
@@ -40,20 +40,106 @@ var composeN = function composeN() {
     };
 };
 
-/**
-   * Typing effect
-   */
-$('.typing__module').each(function () {
+// ##########################
+// Typing
+// ##########################
+$('.typing__module').each(function (index) {
     var self = $(this),
         _wrapper = $('.typed', self)[0],
         optData = eval('(' + self.attr('data-options') + ')'),
         optDefault = {
             stringsElement: self.find('.typed-strings')[0],
             typeSpeed: 50,
-            backSpeed: 500,
-            fadeOut: true,
+            backSpeed: 50,
+            fadeOut: false,
+            smartBackspace: true,
             loop: true
         },
         options = $.extend(optDefault, optData);
     var typed = new Typed(_wrapper, options);
+});
+
+// ##########################
+// Header & Navbar
+// ##########################
+$('.fullscreenmenu__module').each(function () {
+    var self = $(this),
+        triggerID = self.attr('trigger');
+
+    self.on("click", function () {
+        $(triggerID).toggleClass('open');
+        $(this).toggleClass('open');
+    });
+    $(triggerID).on("click", function () {
+        $(triggerID).toggleClass('open');
+        self.toggleClass('open');
+    });
+});
+
+var wh = $(window).height(),
+    half = wh / 5,
+    headerHeight = $('header').outerHeight();
+
+$(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+
+    if (scrollTop >= half) {
+        $('header').addClass('is-scroll');
+    } else {
+        $('header').removeClass('is-scroll');
+    }
+});
+
+$('.onepage-nav').dropdownMenu({
+    menuClass: 'onepage-menu',
+    breakpoint: 1200,
+    toggleClass: 'active',
+    classButtonToggle: 'navbar-toggle',
+    subMenu: {
+        class: 'sub-menu',
+        parentClass: 'menu-item-has-children',
+        toggleClass: 'active'
+    }
+});
+
+$('.onepage-nav').onePageNav({
+    currentClass: 'current-menu-item',
+    scrollOffset: headerHeight
+});
+
+// ##########################
+// SVG Re-colouring
+// ##########################	
+jQuery('img.svg').each(function () {
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function (data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if (typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if (typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass + ' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
 });
